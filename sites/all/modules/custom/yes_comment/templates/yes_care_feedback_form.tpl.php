7cabae6d1efbef71b415c1b1b2e6c799e855b777
hccadmin <dave_rothfarb@dfci.harvard.edu>
2015-08-24 16:29:52 -0400

    Created custom module for care feedback form.

---
+++ sites/all/modules/custom/yes_comment/templates/yes_care_feedback_form.tpl.php
@@ -0,0 +1,18 @@
+<h2>Speak up for better health care!</h2>
+<p>We want to hear what you think about your health care and services under the One Care plan--including both positive and negative experiences you’ve had.</p>
+<p>Your feedback will help One Care providers know if they are meeting your needs. It will also help them to improve the care they provide. </p>
+<p><strong>Your comments are private.</strong> Health care providers will not be able to log in to this website and will not know who gave us feedback.</p>
+<p>If you can, please let us know:
+<div class="feedback-form callout-padding">
+  <?php print render( $form['provider_name'] ); ?>
+  <?php print render( $form['provider_practice'] ); ?>
+  <p>If you don't have this information, we still want your comments</p>
+  <?php print render( $form['feedback_box'] ); ?>
+  <p><small><em>YES Health does not resolve individual health care or One Care disputes or problems directly. </em></small></p>
+  <?php print render( $form['buttons']['submit'] ); ?>
+  <?php print drupal_render_children($form); ?>
+</div>
+<p>You can also comment by phone or email if you would prefer.</p> 
+<p><strong>Call us at:</strong> 617-307-7374<br />
+<strong>Email us at:</strong> <a href="mailto:YEShealth@dpcma.org">YEShealth@dpcma.org</a></p>
+<p>If you are having a problem with your care, please call the One Care Ombudsman’s office at 617-307-7775 or 855-781-9898. </p>
