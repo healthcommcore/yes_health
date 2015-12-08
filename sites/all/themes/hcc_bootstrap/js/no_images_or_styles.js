64e35a0c4e8c065ea017bb51bd9ca99bd2324ac7
hccadmin <dave_rothfarb@dfci.harvard.edu>
2015-12-08 17:55:24 -0500

    Stuck on the cookies functionality

--- sites/all/themes/hcc_bootstrap/js/no_images_or_styles.js
+++ sites/all/themes/hcc_bootstrap/js/no_images_or_styles.js
@@ -7,19 +7,50 @@
     var styles = $("link[type='text/css']");
     var images = $('img');
     var trigger = $('#disable-visuals');
-    $(trigger).data('visuals', true).text(message.on);;
+    $(trigger).data('visuals', 1).text(message.on);;
+    /*
+    */
+    var cookie = cookie_exists();
+
+    if (cookie) {
+      manipulate_visuals(cookie);
+    }
+    else {
+      document.cookie = 'website_visuals=1';
+      $(trigger).data('visuals', 1).text(message.on);
+    }
+
+
     $(trigger).on('click', function (e) {
       e.preventDefault();
-      if( $(trigger).data('visuals') ) {
+      if( $(trigger).data('visuals' === 1) ) {
+        manipulate_visuals(1);
+      }
+      else {
+        manipulate_visuals(0);
+      }
+    });
+
+    function cookie_exists () {
+      var re = RegExp(/website_visuals=[\d]/);
+      var all_cookies = document.cookie;
+      var result = re.exec(all_cookies);
+      return result[0].split('=').pop();
+    }
+
+    function manipulate_visuals(option) {
+      if (option === 1) {
         $(styles).remove();
         $(images).hide();
-        $(trigger).data('visuals', false).text(message.off);;
+        $(trigger).data('visuals', 0).text(message.off);;
+        document.cookie = 'website_visuals=0';
       }
       else {
         $('head').append(styles);
         $(images).show();
-        $(trigger).data('visuals', true).text(message.on);
+        $(trigger).data('visuals', 1).text(message.on);
+        document.cookie = 'website_visuals=1';
       }
-    });
+    }
   });
 })(jQuery);
