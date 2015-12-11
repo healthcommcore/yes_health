09916e639d8ae529565d3f39dd5f87c725376e60
hccadmin <dave_rothfarb@dfci.harvard.edu>
2015-12-11 12:17:24 -0500

    On the right track but the trigger toggling part does not work.

--- sites/all/themes/hcc_bootstrap/js/no_images_or_styles.js
+++ sites/all/themes/hcc_bootstrap/js/no_images_or_styles.js
@@ -1,21 +1,35 @@
 (function($) {
-  $(window).on('load unload', function() {
-    var message = {
-      on: 'Turn off all styles and images',
-      off: 'Turn on all styles and images'
-    };
-    var styles = $("link[type='text/css']");
-    var images = $('img');
-    var trigger = $('#disable-visuals');
-    var cookie = cookie_exists();
-    $(trigger).text(message.on);;
+  var message = {
+    on: 'Turn off all styles and images',
+    off: 'Turn on all styles and images'
+  };
+  var styles = $("link[type='text/css']");
+  var images = $('img');
+  var cookie = cookie_exists();
+  $(window).on('load', function() {
     if(cookie) {
       //$("link[type='text/css']").remove();
       //$('img').hide();
       manipulate_visuals(cookie);
-      console.log('1: Cookie: ' + cookie);
+      //console.log('1: Cookie: ' + cookie);
 
     }
+  });
+  /*
+  */
+  $(document).ready(function() {
+  var trigger = $('#disable-visuals');
+  var triggerCookie = cookie_exists();
+
+    $(trigger).on('click', function (e) {
+      e.preventDefault();
+      var option = (triggerCookie == 1 ? 0 : 1);
+      console.log('2: Click option: ' + option + ', Cookie: ' + triggerCookie);
+      manipulate_visuals(option);
+    });
+  });
+
+
     /*
     console.log(cookie);
     manipulate_visuals(cookie);
@@ -39,12 +53,6 @@
     });
     */
 
-    $(trigger).on('click', function (e) {
-      e.preventDefault();
-      var option = (cookie == 1 ? 0 : 1);
-      console.log('2: Click option: ' + option + ', Cookie: ' + cookie);
-      manipulate_visuals(option);
-    });
 
     function cookie_exists () {
       var re = RegExp(/website_visuals=[\d]/);
@@ -62,22 +70,22 @@
     }
 
     function manipulate_visuals(option) {
-      if (option == 1) {
+      console.log(option);
+      if (option == 0) {
         $(styles).remove();
         $(images).hide();
-        $(trigger).text(message.off);;
+        //$(trigger).text(message.off);;
         document.cookie = 'website_visuals=0';
         //console.log('Cookie changed: 0');
       }
       else {
         $('head').append(styles);
         $(images).show();
-        $(trigger).text(message.on);
+        //$(trigger).text(message.on);
         document.cookie = 'website_visuals=1';
         //console.log('Cookie changed: 1');
       }
     }
     /*
     */
-  });
 })(jQuery);
