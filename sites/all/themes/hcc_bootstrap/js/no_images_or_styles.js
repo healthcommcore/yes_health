aee590f380c2d29f93201e8fa1065db7cbe7289d
hccadmin <dave_rothfarb@dfci.harvard.edu>
2015-12-11 14:51:01 -0500

    Oh my God I think it is finally working

--- sites/all/themes/hcc_bootstrap/js/no_images_or_styles.js
+++ sites/all/themes/hcc_bootstrap/js/no_images_or_styles.js
@@ -19,40 +19,14 @@
   */
   $(document).ready(function() {
   var trigger = $('#disable-visuals');
-  var triggerCookie = cookie_exists();
 
     $(trigger).on('click', function (e) {
       e.preventDefault();
-      var option = (triggerCookie == 1 ? 0 : 1);
-      console.log('2: Click option: ' + option + ', Cookie: ' + triggerCookie);
+      var option = (cookie_exists() == 1 ? 0 : 1);
+      console.log('Trigger: Click option: ' + option + ', Cookie: ' + cookie_exists());
       manipulate_visuals(option);
     });
   });
-
-
-    /*
-    console.log(cookie);
-    manipulate_visuals(cookie);
-
-
-
-    if (cookie) {
-      manipulate_visuals(cookie);
-      //console.log('Cookie exists: ' + cookie);
-    }
-    else {
-      document.cookie = 'website_visuals=1';
-      cookie = 1;
-      $(trigger).text(message.on);
-      //console.log('Cookie DOES NOT exist: ' + cookie);
-    }
-
-    $(window).on('load', function () {
-      console.log(cookie);
-      //manipulate_visuals(cookie);
-    });
-    */
-
 
     function cookie_exists () {
       var re = RegExp(/website_visuals=[\d]/);
@@ -70,21 +44,20 @@
     }
 
     function manipulate_visuals(option) {
-      console.log(option);
+      //console.log(option);
       if (option == 0) {
         $(styles).remove();
         $(images).hide();
         //$(trigger).text(message.off);;
         document.cookie = 'website_visuals=0';
-        //console.log('Cookie changed: 0');
       }
       else {
         $('head').append(styles);
         $(images).show();
         //$(trigger).text(message.on);
         document.cookie = 'website_visuals=1';
-        //console.log('Cookie changed: 1');
       }
+        console.log('Manipulate: Cookie: ' + cookie_exists());
     }
     /*
     */
